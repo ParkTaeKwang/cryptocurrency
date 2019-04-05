3f9d2666414a762027bbf489c25fd1c2c12165fa
ParkTaeKwang <46703698+ParkTaeKwang@users.noreply.github.com>
2019-04-05 18:02:48 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,47 @@
 'use strict';
 
-var request = require('request');
 var express = require('express');
+var parseurl = require('parseurl')
+var session = require('express-session'); // 세션정보는 메모리에 저장함
+var bcrypt = require('bcrypt');
 var router = express.Router();
-var session = require('express-session');
-var cookie = require('cookie-parser'); 
-const app = express()
 
-const bcrypt = require('bcrypt');
+var app = express();
+
+router.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+router.use(function (req, res, next) {
+    if (!req.session.views) {
+        req.session.views = {}
+    }
+    // get the url pathname
+    var pathname = parseurl(req).pathname
+    // count the views
+    req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+
+    next()
+})
 
 
-// render -> login
+router.get('/foo', function (req, res, next) {
+    res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+})
+
+router.get('/bar', function (req, res, next) {
+    res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+})
+
 router.get('/', (req, res) => {
-    /*
-    var session = require('express-session');
-
-    app.use(session({
-        secret: '@#@$MYSIGN#@$#$',
-        resave: false,
-        saveUninitialized: true
-    }));
-
-    var sess = req.session;
-    console.log(sess.secret);
-    */
     res.sendfile('public/login.html');
 });
 
-// API -> login
 router.post('/login', async (req, res) => {
-    /*
-    if (req.session.user) {
-        res.sendfile('public/main.html');
-    }
-    else {
-    */
+
+    res.sendfile('public/login.html');
     var idPass = "$2b$05$YKrQhVu5Hai5ml4sIUmoIue2jTYmgK2mQDRemS3NqeteJhYZRxpKq"; // hash("aaaa");
     var input_id = req.body['email'];
     var input_pw = req.body['password'];
@@ -42,20 +49,18 @@ router.post('/login', async (req, res) => {
     const result = bcrypt.compareSync(input_pw, idPass);  // input pw if hash("aaaa");
 
     try {
-        if (input_id == 'admin' && result == true) {    
-            req.session.email = {
-                
-                email: email
-            };
-            res.json({ 'result': req.session.email });
-            } else {
-                res.json({ 'result': 'false' });
-            }
+        if (input_id == 'admin' && result == true) {
+            res.json({ 'result': 'true' });
+        } else {
+            res.json({ 'result': 'false' });
+        }
     } catch (error) {
-           console.error(error);
-           return next(error);
-     } 
+        console.error(error);
+        return next(error);
+    } 
 
-});
+
+})
 
 module.exports = router;
+
